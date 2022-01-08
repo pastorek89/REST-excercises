8b56a91f3101d7684a8f53bc84abaca7c442ecf4
pastorek89 <pastor.patryk@gmail.com>
2022-01-08 22:20:28 +0100

    Dynamic filtering in RestController

--- src/main/java/com/in28minutes/pastor/rest/webservices/filtering/BeanToFilter.java
+++ src/main/java/com/in28minutes/pastor/rest/webservices/filtering/BeanToFilter.java
@@ -2,14 +2,17 @@ package com.in28minutes.pastor.rest.webservices.filtering;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 //Two ways of static ignore properties in response, however for me @JsonIgnore is a better one, because @JsonIgnoreProperties is hard-coded.
-@JsonIgnoreProperties(value = "date")
+//@JsonIgnoreProperties(value = "date")
+//Dynamic filtering in FilteringController
+@JsonFilter(value = "BeanToFilterFilter")
 public class BeanToFilter {
 
 	private String name;
-	@JsonIgnore
+	//@JsonIgnore
 	private int value;
 	private Date date;
 	
