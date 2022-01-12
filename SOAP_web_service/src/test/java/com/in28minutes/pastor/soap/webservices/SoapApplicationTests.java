654fad7ce190a508892ce67a7e893417b710f4ed
pastorek89 <pastor.patryk@gmail.com>
2022-01-12 20:42:42 +0100

    New project for SOAP learning

---
+++ SOAP_web_service/src/test/java/com/in28minutes/pastor/soap/webservices/SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.pastor.soap.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
